da9bb5138f5435081b65738c2f04e64c798ac708
David Hong <phazex1@gmail.com>
2021-09-21 23:05:18 -0400

    Adding advanced problem answers

--- lab.js
+++ lab.js
@@ -232,7 +232,26 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
-
+switch (letterGrade) {
+  case "A": 
+    console.log ("The student is doing excellently.")
+    break;
+  case "B":
+    console.log ("The student is doing well.")
+    break;
+  case "C":
+    console.log ("The student is doing alright.")
+    break;
+  case "D":
+    console.log ("The student is not doing very well.")
+    break;
+  case "F":
+    console.log ("The student is failing.")
+    break;
+  default: 
+    console.log ("Not an eligible grade.")
+    break;
+}
 
 
 ////////// PROBLEM 14 //////////
@@ -259,4 +278,14 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
   ...
 */
 
-//Code Here
+for (let i = 0; i < 101; i++) {
+  if (i % 3 === 0){
+    console.log ("Dev")
+  } else if ( i % 5 === 0) {
+    console.log ("mountain")
+  } else if (i % 3 === 0 && i % 5 === 0){
+    console.log ("Devmountain")
+  } else {
+    console.log(i)
+  }
+}
